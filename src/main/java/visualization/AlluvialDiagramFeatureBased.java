93c93fecc86f2de6aa64658e6a29e7fa18ca685a
letimome <leticia.montalvillo@ehu.es>
2017-08-22 18:50:02 +0200

    getting rid of Ecore genererated packages. Just for now..

---
+++ src/main/java/visualization/AlluvialDiagramFeatureBased.java
@@ -0,0 +1,13 @@
+package visualization;
+
+import java.util.ArrayList;
+
+import SPLconcepts.CoreAssetBaseline;
+import SPLconcepts.CustomizationDetail;
+
+public class AlluvialDiagramFeatureBased {
+	
+	CoreAssetBaseline CABaseline;
+	ArrayList<CustomizationDetail> customizations;
+	
+}
